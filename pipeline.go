269bc589918a4ca3f8357ed32fd2fc2439c8f30f
Ryan Puskas <rpuskas@infoarmor.com>
2020-08-12 19:43:21 -0400

    changes to deployment folder

--- pipeline.go
+++ pipeline.go
@@ -59,25 +59,11 @@ func generatePipeline(steps []interface{}, projects []Project) *Pipeline {
 		stepMap, _ := step.(map[interface{}]interface{})
 		env, _ := stepMap["env"].(map[interface{}]interface{})
 		value, ok := env["BUILDPIPE_SCOPE"]
-		if ok && value == "distinct" 
-			projectSteps := generateDistinctProjectSteps(step, projects)
-			for _, ps := range projectSteps {
-				skip := false
-				psMap, _ := ps.(map[interface{}]interface{})
-				psLabel, _ := psMap["label"]
-				for _, gs := range generatedSteps {
-					gsMap, _ := gs.(map[interface{}]interface{})
-					gsLabel, _ := gsMap["label"]
-					if gsLabel == psLabel {
-						skip = true
-			        }
-				} 
-				if(!skip) {
-					generatedSteps = append(generatedSteps, ps)
-				}
-			}
+		if ok && value == "distinct" {
+			projectSteps := generateProjectSteps(step, projects)
+			generatedSteps = append(generatedSteps, projectSteps...)
 		} else {
-			errors.New("Must be distinct")
+			errors.New("Must be a project or distinct")
 		}
 	}
 
